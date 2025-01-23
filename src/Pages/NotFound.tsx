a12daf2fd64416c135e5488afbeff57580f064b7
LUIGI-CTO <luigicto286@gmail.com>
2025-01-23 13:17:17 +0100

    works?

--- src/Pages/NotFound.tsx
+++ src/Pages/NotFound.tsx
@@ -3,12 +3,12 @@
 export const NotFound = () => {
   return (
     <div className="flex h-screen flex-col items-center justify-center text-center text-[--secondary-color]">
-      <h1 className="text-4xl font-bold text-[--secondary-color]">Stay tuned!</h1>
+      <h1 className="text-4xl font-bold text-[--secondary-color]">404 PAGE NOT FOUND</h1>
       <a
         href="/"
         className="mt-4 rounded-lg bg-[--secondary-color] px-4 py-2 text-[--primary-color] hover:bg-opacity-80"
       >
-        More is coming soon...
+        Don't know how you end up here... Just buy $LUIGI
       </a>
     </div>
   );
